2e0883fcdefab0bac2e5d2d85782215013221a57
Samiksha Moghan <samikshamoghan@gmail.com>
2026-03-19 15:17:25 +0800

    Auth Context & Protected Routing Logic

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,4 +1,20 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext";
+
+
+
 export default function Profile() {
+  const {user, isLoading} = useAuth();
+  const plan = true;
+
+  if (!user && !isLoading) {
+    return <Navigate to="/auth/sign-in" replace />;
+  }
+
+  if(plan) {
+    return <Navigate to="/onboarding" replace />;
+  }
+  
   return (
     <>
       <h1>Profile</h1>
